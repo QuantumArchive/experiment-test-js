97f407c51cd0ad625775315e2652d2500d8512f3
Christopher Bruner <chrbruner9@gmail.com>
2016-10-10 09:51:10 -0700

    test working data

--- scripts/model/model.js
+++ scripts/model/model.js
@@ -1,1 +1,32 @@
 'use strict';
+
+(function(module) {
+  var uberInfo = {};
+
+  uberInfo.allDrivers = [];
+
+  uberInfo.getOauth = function() {
+    var tempString = '?latitude=45.5231&longitude=-122.6765';
+    var ajaxQuery = {
+      //url: 'http://localhost:3000/uber/',
+      url: 'https://api.uber.com/v1/products?latitude=45.5231&longitude=-122.6765',
+      type: 'GET',
+      headers: {
+        Authorization: 'Token '
+      },
+      success: function(data) {
+        console.log(data);
+      },
+      error : function(data, status, xhr) {
+        console.log(data);
+        console.log(status);
+        console.log(xhr);
+        console.log('What happened?');
+      }
+    };
+    $.ajax(ajaxQuery);
+  };
+
+  module.uberInfo = uberInfo;
+  uberInfo.getOauth();
+})(window);
--- server.js
+++ server.js
@@ -5,6 +5,19 @@ var express = require('express'),
   port = process.env.PORT || 3000,
   app = express();
 
+function proxyUber(request, response) {
+  console.log('Routing Uber request for ', request.params[0]);
+  console.log(process.env.UBER_TOKEN);
+  (requestProxy({
+    url: 'https://api.uber.com/v1/products?latitude=45.5231&longitude=-122.6765',
+    headers: {
+      Authorization: 'Token ' + process.env.UBER_TOKEN
+    }
+  }))(request, response);
+};
+
+//app.get('/uber/*', proxyUber);
+
 app.use(express.static('./'));
 
 app.get('*', function(request, response) {
